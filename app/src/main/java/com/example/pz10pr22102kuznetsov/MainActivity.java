2771c25e53f341ca81658bc1d6ead2b117ac210f
Reshala57339 <Max.san.2005@mail.ru>
2024-06-20 18:33:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pz10pr22102kuznetsov/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pz10pr22102kuznetsov;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Draw2D draw2D = new Draw2D(this);
+        setContentView(draw2D);
+    }
+}
